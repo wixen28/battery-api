2a5534f7fe9937903a10b41b24e1fa61f9fcba82
Viktoria <115079673+wixen28@users.noreply.github.com>
2023-05-02 15:47:35 +0200

    Add files via upload

---
+++ battery-api/script.js
@@ -0,0 +1,80 @@
+
+
+const batteryLevel = document.querySelector(".batteryLevel");
+const batteryCharging = document.querySelector(".batteryCharging");
+const batteryChargingTime = document.querySelector(".batteryChargingTime");
+const batteryDisChargingTime = document.querySelector(
+  ".batteryDisChargingTime"
+);
+const batteryStatus = document.querySelector('.batteryStatus')
+
+
+//Battery API
+
+const battery = () => {
+    if('getBattery' in navigator) {
+      navigator.getBattery().then((battery) => {
+          function updateAllBatteryDetails() {
+             updateChargingInfo()
+             updateLevelChange()
+             updateDischargingTimeInfo()
+             updateChargingTimeChangeInfo()
+             updateBatteryStatus()
+           }
+
+           updateAllBatteryDetails()
+          //battery charging change
+          battery.addEventListener('chargingchange', function(e) {
+            updateChargingInfo()
+          })
+
+         function updateChargingInfo() {
+                const isCharging = battery.charging ? 'Yes' : 'No'
+                batteryCharging.innerHTML = isCharging
+          }
+          //battery charging time
+          battery.addEventListener('chargingtimechange', () => {
+            updateChargingTimeChangeInfo()
+          })
+
+          function updateChargingTimeChangeInfo() {
+            console.log(battery.chargingTime)
+            batteryChargingTime.innerHTML = battery.chargingTime + " seconds"
+          }
+          // battery discharging time
+          battery.addEventListener('dischargingtimechange', () => {
+            updateDischargingTimeInfo()
+          })
+
+          function updateDischargingTimeInfo() {
+            batteryDisChargingTime.innerHTML =  battery.dischargingTime + " seconds"
+          }
+
+          //battery level change
+          battery.addEventListener('levelchange', () => {
+            updateLevelChange()
+          })
+
+          function updateLevelChange() {
+            const level = battery.level * 100 + "%" 
+            batteryLevel.innerHTML = level
+        
+          }
+          // battery status
+          function updateBatteryStatus() {
+            const level = battery.level * 100/3
+            if(level) {
+              batteryStatus.innerHTML = 'Fully Charged'
+            } else if(level < 50){
+              batteryStatus.innerHTML = 'Low battery'  
+            }
+          }
+      })
+    }
+    
+}
+
+
+
+battery()
+
